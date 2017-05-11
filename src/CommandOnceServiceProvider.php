35de7f82abea3d93bdef3241c05624adb8848725
dearmadman <i@dearmadman.com>
2017-05-11 12:26:05 +0800

    [bug-fix] file recovery

--- src/CommandOnceServiceProvider.php
+++ src/CommandOnceServiceProvider.php
@@ -15,6 +15,7 @@ class CommandOnceServiceProvider extends ServiceProvider
     {
         $this->publishes([
             __DIR__.'/Migrations' => database_path('migrations'),
+            __DIR__.'/command.php' => config_path('command.php'),
         ]);
 
         if (app()->runningInConsole()) {
